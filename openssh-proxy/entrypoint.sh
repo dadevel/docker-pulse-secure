a9cfbaf33fffeb1e8905af896204a3f98253c85f
dadevel <dadevel@disroot.org>
2020-04-02 19:54:53 +0200

    test for users by id instead of name

--- openssh-proxy/entrypoint.sh
+++ openssh-proxy/entrypoint.sh
@@ -5,8 +5,8 @@ USER_ID="${USER_ID:-1000}"
 GROUP_ID="${GROUP_ID:-$USER_ID}"
 
 # create user with same uid and gid as outside the container
-getent group proxy &>/dev/null || addgroup -g "$GROUP_ID" proxy
-getent passwd proxy &>/dev/null || adduser -u "$USER_ID" -g proxy -G proxy -D -h /data -H proxy
+getent group "$GROUP_ID" &> /dev/null || addgroup -g "$GROUP_ID" proxy
+getent passwd "$USER_ID" &> /dev/null || adduser -u "$USER_ID" -g proxy -G proxy -D -h /data -H proxy
 
 # fix ownership and permissions
 mkdir -p /data/.ssh
@@ -15,7 +15,7 @@ touch /data/.ssh/authorized_keys
 chmod 600 /data/.ssh/authorized_keys
 chown -R "$USER_ID:$GROUP_ID" /data
 
-# set random password
+# set random password, user must be enabled to allow ssh login
 echo "proxy:$(tr -dc [:alnum:] < /dev/urandom | head -c 32)" | chpasswd
 
 exec /usr/sbin/sshd -D -e "$@"
--- pulse-secure-client/entrypoint.sh
+++ pulse-secure-client/entrypoint.sh
@@ -5,16 +5,15 @@ USER_ID="${USER_ID:-1000}"
 GROUP_ID="${GROUP_ID:-$USER_ID}"
 
 # create user with same uid and gid as outside the container
-getent group pulse &> /dev/null || groupadd --gid "$GROUP_ID" pulse
-getent passwd pulse &> /dev/null || useradd --uid "$USER_ID" --gid "$GROUP_ID" --comment pulse --home-dir /data --no-create-home pulse
+getent group "$GROUP_ID" &> /dev/null || groupadd --gid "$GROUP_ID" pulse
+getent passwd "$USER_ID" &> /dev/null || useradd --uid "$USER_ID" --gid "$GROUP_ID" --comment pulse --home-dir /data --no-create-home pulse
 
 # fix ownership
-mkdir -p /data/.pulse_secure/pulse
-touch /data/.pulse_secure/pulse/pulsesvc.log /data/.pulse_secure/pulse/.pulse_Connections.txt
+mkdir -p /data
 chown -R "$USER_ID:$GROUP_ID" /data
 
 # docker always bind mounts /etc/hosts, but pulse insists in moving this file
-# container must run in privileged mode otherwise unmount will fail
+# container must have CAP_SYS_ADMIN otherwise unmount will fail
 if mountpoint -q /etc/hosts; then
     cp /etc/hosts /tmp/
     umount /etc/hosts
