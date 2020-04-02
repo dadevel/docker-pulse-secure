b02ba138f563b11bf2e6cb2a682899e2d7e87ed7
dadevel <dadevel@disroot.org>
2020-04-02 21:48:07 +0200

    implement tunnel keep-alive for pulse secure

--- pulse-secure-client/entrypoint.sh
+++ pulse-secure-client/entrypoint.sh
@@ -9,8 +9,21 @@ getent group "$GROUP_ID" &> /dev/null || groupadd --gid "$GROUP_ID" pulse
 getent passwd "$USER_ID" &> /dev/null || useradd --uid "$USER_ID" --gid "$GROUP_ID" --comment pulse --home-dir /data --no-create-home pulse
 
 # fix ownership
-mkdir -p /data
+mkdir -p /data/.pulse_secure/pulse
+touch /data/.pulse_secure/pulse/pulsesvc.log
 chown -R "$USER_ID:$GROUP_ID" /data
+
+tail -f /data/.pulse_secure/pulse/pulsesvc.log | while read -r line; do
+    # cut off first two blocks of the log message
+    line="${line#* * }"
+    echo "${line}"
+    # keep the tunnel open by sending a ping message every 5 min to the gateway
+    if [[ "${line}" =~ ^adapter.info\ cip\ =\ [^,]+,\ mask\ =\ [^,]+,\ gw\ =\ ([^,]+), ]]; then
+        ping -i 300 -- "${BASH_REMATCH[1]}" | while read -r line; do
+            echo "keepalive.info ${line}"
+        done &
+    fi
+done &
 
 # docker always bind mounts /etc/hosts, but pulse insists in moving this file
 # container must have CAP_SYS_ADMIN otherwise unmount will fail
@@ -20,6 +33,5 @@ if mountpoint -q /etc/hosts; then
     mv /tmp/hosts /etc/
 fi
 
-cd /data
-exec su -c /pulse-wrapper.sh pulse
+exec su -c /usr/local/pulse/pulseUi pulse
 
--- pulse-secure-client/pulse-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-tail -f ~/.pulse_secure/pulse/pulsesvc.log &
-/usr/local/pulse/pulseUi &
-wait $!
-
